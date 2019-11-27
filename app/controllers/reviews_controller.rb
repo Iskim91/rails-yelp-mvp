9d8aff6683a846da264a35057e98143e93de5ac8
Isac Kim <jk724814@gmail.com>
2019-11-27 14:42:46 +0100

    completed the connection Restaurant and reviews

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,18 @@
+class ReviewsController < ApplicationController
+  def new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new
+  end
+
+  def create
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    review_params = params.require(:review).permit(:content, :rating)
+    @review = Review.new(review_params)
+    @review.restaurant = @restaurant
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# destroy all of them
+Restaurant.destroy_all
+
+# create the restaurants
+Restaurant.create(name: 'apple', address: 'gracia', phone_number: '12 12 12 12', category: 'belgian')
+Restaurant.create(name: 'banana', address: 'idk', phone_number: '13 131 12 2', category: 'chinese')
+Restaurant.create(name: 'orange', address: 'montjiuc', phone_number: '222222222', category: 'japanese')
+Restaurant.create(name: 'pineapple', address: 'joanic', phone_number: '333333333', category: 'french')
+Restaurant.create(name: 'avocado', address: 'tetuan', phone_number: '44444444', category: 'italian')
+
+# count the restaurants
+puts Restaurant.count
